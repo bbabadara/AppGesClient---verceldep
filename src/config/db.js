e9de247d2a70fe369b7c44166c8596d55e0fd4d2
bbabadara <bbabadara@gmail.com>
2025-08-07 00:15:58 +0000

    v3

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,12 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    if (!mongoose.connection.readyState) {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log('MongoDB connected');
-    }
-  } catch (error) {
-    console.error('MongoDB connection error:', error.message);
+    await mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
     process.exit(1);
   }
 };
